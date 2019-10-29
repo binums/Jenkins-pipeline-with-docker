61b354e8a9a7d236ed939540f66a83c0c2a4310d
root <root@ip-172-31-39-75.ec2.internal>
2019-10-29 17:09:01 +0000

    pre-testing

--- pipeline-on-ubuntu/serve.sh
+++ pipeline-on-ubuntu/serve.sh
@@ -5,11 +5,12 @@ docker container exec pipelineonubuntu_jekns-ans-master_1 sshpass -p "1234" ssh 
 docker container exec pipelineonubuntu_jekns-ans-master_1 sshpass -p "1234" ssh -o StrictHostKeyChecking=no remote-user@172.24.0.8 'exit'
 docker container exec pipelineonubuntu_jekns-ans-master_1 sshpass -p "1234" ssh -o StrictHostKeyChecking=no remote-user@172.24.0.10 'exit'
 echo -e "\n fetching jenkins password........"
-sleep 20
+sleep 30
 echo -e "\nJenkins password:"
 docker container exec pipelineonubuntu_jekns-ans-master_1 cat /var/jenkins_home/secrets/initialAdminPassword
 echo -e "\n fetching nexus password........"
-sleep 10
+wget -qO- $(hostname --ip-address):8081 &> /dev/null
+sleep 50
 echo -e "\nNexus Password:"
 docker container exec pipelineonubuntu_nexus_1 cat /nexus-data/admin.password
 echo -e "\n\n"
